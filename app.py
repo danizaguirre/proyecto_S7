045c3d925d333507d99d271db730f7b480cc4b12
danizaguirre <ortodoin@gmail.com>
2026-01-02 13:57:25 -0600

    actualizacion de archivo app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+df = pd.read_csv('vehicles_us.csv')  # leer los datos
 # Primer botón - Histograma
 hist_button = st.button('Construir histograma')
 if hist_button:
