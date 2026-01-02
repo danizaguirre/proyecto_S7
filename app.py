8c41c45e4af51ac9c057b4252334d607a7f8a16c
danizaguirre <ortodoin@gmail.com>
2026-01-02 14:20:59 -0600

    Enhance app.py with data analysis features
    Added headers and buttons for histogram and scatter plot.

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import plotly.express as px
 import streamlit as st
 
 df = pd.read_csv('vehicles_us.csv')  # leer los datos
+# Encabezado
+st.title("Análisis de datos de vehículos en EU")
 # Primer botón - Histograma
+st.header("Histograma de kilometraje")
 hist_button = st.button('Construir histograma')
 if hist_button:
     st.write(
@@ -12,6 +15,7 @@ if hist_button:
     st.plotly_chart(fig, use_container_width=True)
 
 # Segundo botón - Gráfico de dispersión
+st.header("Dispersión tipo de auto/precio")
 scatter_button = st.button('Construir gráfico de dispersión')
 if scatter_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
